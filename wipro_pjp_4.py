dd47c2374d8520dce3d4de35aff1108b1e3bc149
Vars-ah <66832417+Vars-ah@users.noreply.github.com>
2021-07-18 19:19:21 +0530

    Add files via upload
    I/O Programs written in PJP

---
+++ wipro_pjp_4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+FNAME = str(input('Enter -->'))
+LNAME = str(input('Enter -->'))
+print(FNAME + LNAME)
+
+
+# In[5]:
+
+
+num1 = int(input('Enter -->'))
+num2 = int(input('Enter -->'))
+sum = num1 + num2
+print(sum)
+
+
+# In[7]:
+
+
+a = str(input('Enter -->'))
+b = int(input('Enter -->'))
+c = float(input('Enter -->'))
+d = eval(input('Enter -->'))
+print(a, b, c, d)
+
